07aac9de318dd042354ae4bbaada423eb41a30a5
Kaoru NAKAMURA <kaorun55@gmail.com>
2017-02-24 16:55:06 +0900

    batファイルの調整

--- convert.bat
+++ convert.bat
@@ -2,21 +2,20 @@ rem rmdir output /s /q
 mkdir output
 
 xcopy "Chapter 1" "output" /e /y
-xcopy "Chapter 2" "output" /e /y
-xcopy "Chapter 3" "output" /e /y
-xcopy "Chapter 4" "output" /e /y
-xcopy "Chapter 5" "output" /e /y
-xcopy "Chapter 6" "output" /e /y
 
 cd output 
-pandoc --reference-docx=..\reference.docx -o HoloLensProgrammingBook.docx Chapter1.md 
 
-move TeamDevBook.docx ..\TeamDevBook.docx
+"C:\Program Files (x86)\Pandoc\pandoc" --reference-docx=..\reference.docx -o HoloLensProgrammingBook.docx Chapter1.md 
+
+move HoloLensProgrammingBook.docx ..\HoloLensProgrammingBook.docx
 
 cd ..\
 
 powershell .\convert.ps1
  
 rmdir /S /Q output
+
+move /Y "HoloLensProgrammingBook.docx" "C:\Users\kaoru\Dropbox\__送付物\HoloLensProgrammingBook"
+move /Y "HoloLensProgrammingBook.pdf" "C:\Users\kaoru\Dropbox\__送付物\HoloLensProgrammingBook"
 
 pause
