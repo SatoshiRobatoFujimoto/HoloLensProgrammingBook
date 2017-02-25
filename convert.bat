182c5807638e68535041b0cfc6760cdbd57958fe
Kaoru NAKAMURA <kaorun55@gmail.com>
2017-02-25 19:03:15 +0900

    改行コードかなにかでおかしくなってたので修正

--- convert.bat
+++ convert.bat
@@ -1,21 +1,47 @@
-rem rmdir output /s /q
+﻿rem rmdir output /s /q
+
+
+
 mkdir output
+
+
+
 
 xcopy "Chapter 1" "output" /e /y
 
+
+
+
 cd output 
+
 
 "C:\Program Files (x86)\Pandoc\pandoc" --reference-docx=..\reference.docx -o HoloLensProgrammingBook.docx Chapter1.md 
 
+
+
+
 move HoloLensProgrammingBook.docx ..\HoloLensProgrammingBook.docx
+
+
+
 
 cd ..\
 
+
 powershell .\convert.ps1
- 
+
+
+
+
 rmdir /S /Q output
 
-move /Y "HoloLensProgrammingBook.docx" "C:\Users\kaoru\Dropbox\__送付物\HoloLensProgrammingBook"
-move /Y "HoloLensProgrammingBook.pdf" "C:\Users\kaoru\Dropbox\__送付物\HoloLensProgrammingBook"
+
+
+
+move /Y "HoloLensProgrammingBook.pdf" "C:\Users\kaoru\Dropbox\Books\HoloLensProgrammingBook"
+
+
+move /Y "HoloLensProgrammingBook.docx" "C:\Users\kaoru\Dropbox\Books\HoloLensProgrammingBook"
+
 
 pause
